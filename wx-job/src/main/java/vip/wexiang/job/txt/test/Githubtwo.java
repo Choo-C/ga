dd8b3a82e40a42bbc6ff125d60d8d46db7c20176
Choo-C <710005130@qq.com>
2023-07-03 02:52:19 +1000

    two github

--- wx-job/src/main/java/vip/wexiang/job/txt/test/Githubtwo.java
+++ wx-job/src/main/java/vip/wexiang/job/txt/test/Githubtwo.java
@@ -5,5 +5,9 @@ public class Githubtwo {
         System.out.println("CodeGeeX");
         System.out.println("two");
         System.out.println("three");
+        System.out.println("five");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
