0de471ff95fe5bd6dbcd06937eb3b05d527d8911
Choo-C <710005130@qq.com>
2023-07-02 19:49:11 +1000

    嘻嘻嘻

--- wx-job/src/main/java/vip/wexiang/job/XxlJobDemoApplication.java
+++ wx-job/src/main/java/vip/wexiang/job/XxlJobDemoApplication.java
@@ -3,10 +3,13 @@ package vip.wexiang.job;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class XxlJobDemoApplication {
     public static void main(String[] args) {
-
+//        故意写的
+        System.out.println(new Date() + ": Starting Xiaolang job demo...");
         SpringApplication.run(XxlJobDemoApplication.class, args);
     }
 }
