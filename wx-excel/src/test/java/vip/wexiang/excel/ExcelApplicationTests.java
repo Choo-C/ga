561fd22a96091af66da2747bfd9ba162ce5a3679
Choo-C <710005130@qq.com>
2023-07-03 05:58:26 +1000

    two github

---
+++ wx-excel/src/test/java/vip/wexiang/excel/ExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.wexiang.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
